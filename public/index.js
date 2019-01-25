5615df3cc1ff08dde3f41ea104471e3701c7d91e
EconClass <bizmail327@gmail.com>
2019-01-25 14:33:47 -0800

    Users can join and send messages

--- public/index.js
+++ public/index.js
@@ -11,8 +11,35 @@ $(document).ready(()=>{
         };
     });
 
+    $('#sendChatBtn').click((e) => {
+        e.preventDefault();
+        // Get the message text value
+        let message = $('#chatInput').val();
+        // Make sure it's not empty
+        if(message.length > 0){
+            // Emit the message with the current user to the server
+            socket.emit('new message', {
+            sender : currentUser,
+            message : message,
+            });
+            $('#chatInput').val("");
+        };
+    });
+
     //socket listeners
     socket.on('new user', (username) => {
         console.log(`✋ ${username} has joined the chat! ✋`);
+        // Add the new user to the online users div
+        $('.usersOnline').append(`<div class="userOnline">${username}</div>`);
     });
-})
+
+    //Output the new message
+    socket.on('new message', (data) => {
+        $('.messageContainer').append(`
+        <div class="message">
+            <p class="messageUser">${data.sender}: </p>
+            <p class="messageText">${data.message}</p>
+        </div>
+        `);
+    });
+});
